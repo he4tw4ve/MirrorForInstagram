2958b8333c476c8b4e83e156b7906e67ffff4093
diegoo_ghz <diego.gallegos.hds@gmail.com>
2025-11-29 16:37:11 -0700

    added banner, deleted cache from repo

---
+++ banner.py
@@ -0,0 +1,64 @@
+from colorama import Fore, Style
+
+def banner1():
+    
+    print(Style.BRIGHT + Fore.YELLOW + r'''
+ __    __   __   ______   ______   ______   ______    
+/\ "-./  \ /\ \ /\  == \ /\  == \ /\  __ \ /\  == \   
+\ \ \-./\ \\ \ \\ \  __< \ \  __< \ \ \/\ \\ \  __<   
+ \ \_\ \ \_\\ \_\\ \_\ \_\\ \_\ \_\\ \_____\\ \_\ \_\ 
+  \/_/  \/_/ \/_/ \/_/ /_/ \/_/ /_/ \/_____/ \/_/ /_/  ''' + Style.RESET_ALL)
+    print(Style.BRIGHT + Fore.MAGENTA + "https://github.com/he4tw4ve/MirrorForInstagram.git\n" + Style.RESET_ALL)
+
+def banner2():
+      UNDERLINE = "\033[4m"
+      print(Style.BRIGHT + Fore.YELLOW + r'''
+    __  ___   ____   ____     ____     ____     ____ 
+   /  |/  /  /  _/  / __ \   / __ \   / __ \   / __ \
+  / /|_/ /   / /   / /_/ /  / /_/ /  / / / /  / /_/ /
+ / /  / /  _/ /   / _, _/  / _, _/  / /_/ /  / _, _/ 
+/_/  /_/  /___/  /_/ |_|  /_/ |_|   \____/  /_/ |_|      ''' + Style.RESET_ALL)
+      print(Style.BRIGHT + Fore.CYAN + UNDERLINE + "https://github.com/he4tw4ve/MirrorForInstagram.git\n" + Style.RESET_ALL)
+
+def banner3():
+      print(Style.BRIGHT + Fore.YELLOW + r'''
+                                                                       
+          ____                                   ,----..               
+        ,'  , `.   ,---,,-.----.   ,-.----.     /   /   \  ,-.----.    
+     ,-+-,.' _ |,`--.' |\    /  \  \    /  \   /   .     : \    /  \   
+  ,-+-. ;   , |||   :  :;   :    \ ;   :    \ .   /   ;.  \;   :    \  
+ ,--.'|'   |  ;|:   |  '|   | .\ : |   | .\ :.   ;   /  ` ;|   | .\ :  
+|   |  ,', |  ':|   :  |.   : |: | .   : |: |;   |  ; \ ; |.   : |: |  
+|   | /  | |  ||'   '  ;|   |  \ : |   |  \ :|   :  | ; | '|   |  \ :  
+'   | :  | :  |,|   |  ||   : .  / |   : .  /.   |  ' ' ' :|   : .  /  
+;   . |  ; |--' '   :  ;;   | |  \ ;   | |  \'   ;  \; /  |;   | |  \  
+|   : |  | ,    |   |  '|   | ;\  \|   | ;\  \\   \  ',  / |   | ;\  \ 
+|   : '  |/     '   :  |:   ' | \.':   ' | \.' ;   :    /  :   ' | \.' 
+;   | |`-'      ;   |.' :   : :-'  :   : :-'    \   \ .'   :   : :-'   
+|   ;/          '---'   |   |.'    |   |.'       `---`     |   |.'     
+'---'                   `---'      `---'                   `---'       
+                                                                       ''' + Style.RESET_ALL)
+      print(Style.BRIGHT + Fore.MAGENTA + "https://github.com/he4tw4ve/MirrorForInstagram.git\n" + Style.RESET_ALL)
+
+def banner4():
+      print(Style.BRIGHT + Fore.YELLOW + r'''
+                                                                       
+___  ___  _____  ______  ______   _____  ______ 
+|  \/  | |_   _| | ___ \ | ___ \ |  _  | | ___ \
+| .  . |   | |   | |_/ / | |_/ / | | | | | |_/ /
+| |\/| |   | |   |    /  |    /  | | | | |    / 
+| |  | |  _| |_  | |\ \  | |\ \  \ \_/ / | |\ \ 
+\_|  |_/  \___/  \_| \_| \_| \_|  \___/  \_| \_|                         ''' + Style.RESET_ALL)
+      print(Style.BRIGHT + Fore.MAGENTA + "https://github.com/he4tw4ve/MirrorForInstagram.git\n" + Style.RESET_ALL)
+
+def banner5():
+      print(Style.BRIGHT + Fore.YELLOW + r'''
+ ___      ___   __      _______    _______     ______     _______   
+|"  \    /"  | |" \    /"      \  /"      \   /    " \   /"      \  
+ \   \  //   | ||  |  |:        ||:        | // ____  \ |:        | 
+ /\\  \/.    | |:  |  |_____/   )|_____/   )/  /    ) :)|_____/   ) 
+|: \.        | |.  |   //      /  //      /(: (____/ //  //      /  
+|.  \    /:  | /\  |\ |:  __   \ |:  __   \ \        /  |:  __   \  
+|___|\__/|___|(__\_|_)|__|  \___)|__|  \___) \"_____/   |__|  \___)  ''' + Style.RESET_ALL)
+      print(Style.BRIGHT + Fore.MAGENTA + "https://github.com/he4tw4ve/MirrorForInstagram.git\n" + Style.RESET_ALL)
+                                                                    
